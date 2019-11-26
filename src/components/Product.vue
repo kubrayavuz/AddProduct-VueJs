7e76c55633a6f943f62c2483146d941fc699b228
Kubra <kubra.yavuz4@ogr.sakarya.edu.tr>
2019-11-26 11:58:25 +0300

    components created

---
+++ src/components/Product.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="col-md-2 card">
+	   <slot></slot>
+  </div>
+</template>
+<script>
+
+	export default{
+		
+		data:function(){
+			return{
+
+			}
+		}
+	}
+</script>
+<style>
+	
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="row product-container">
+		<app-product>
+			 <img class="card-img-top" src="/src/assets/default.png" alt="Card image cap">
+			 <div class="card-body">
+			  <h5 class="card-title">Card title</h5>
+		      <small><strong>Adet : </strong> 1 </small>
+		      <br>
+		      <small><strong>Fiyat : </strong> 10 </small>
+		      <br>
+		      <small><strong>Tutar : </strong> 10 </small>
+		    </div>
+		</app-product>
+	</div>
+</template>
+<script>
+
+import Product from "./Product";
+
+	export default{
+		components:{
+			appProduct : Product
+		},
+		data:function(){
+			return{
+
+			}
+		}
+	}
+</script>
+<style>
+	
+</style>
