04bf790fac76e1820b6c8e9e9e9101cd8c44f232
Kubra <kubra.yavuz4@ogr.sakarya.edu.tr>
2019-11-26 16:57:59 +0300

    product deletion has been made

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,7 +3,7 @@
 		<h3 class="text-center">Added Products</h3>
     	<hr>
 		<div class="row product-container">
-			<app-product v-for="product in productList">
+			<app-product v-for="(product,index) in productList">
 				<img class="card-img-top" :src="product.selectedImage" :alt="product.title">
 				<div class="card-body">
 				 	<h5 class="card-title"> {{ product.title }}</h5>
@@ -11,7 +11,8 @@
 			       	<br>
 			    	<small><strong>Price : </strong> {{product.price}}</small>
 			    	<br>
-			    	<small><strong>Total : </strong> {{ product.totalPrice }}</small>
+			    	<small><strong>Total : </strong> {{ product.totalPrice }}</small>			    	
+			   	    <button class="btn btn-outline-danger btn-block" @click="$delete(productList,index)">Delete</button>
 			    </div>
 			</app-product>
 		</div>
@@ -40,6 +41,11 @@
 					alert("You cannot add more products")
 				}
 			});
+		},
+		watch: {
+			productList() {
+			eventBus.$emit("progressBarUpdated", this.productList.length);
+			}
 		}
 	}
 </script>
